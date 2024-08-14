ce033002f3e448e5a5252ad76c730f2c310c21db
GeorgeJose <george@kumohq.co>
2024-08-14 23:44:11 +0530

    first commit

---
+++ components/ui/Brand/index.jsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+const Brand = () => (
+    <Link href="/">
+        <h1 className="text-2xl font-bold"><span className="text-yellow-500">Bug</span>Builder.</h1>
+    </Link>
+)
+export default Brand
